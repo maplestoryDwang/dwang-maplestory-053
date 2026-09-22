96bce63a6dd2f85c2dcc995a3c2714762951b76b
dwang <e110110eee@gmail.com>
2026-09-22 21:01:09 +0800

    bugfix: 脚本打印日志显示错误

--- gms-server/gms-handler/scripts-zh-CN/template/WildBossTemplate.js
+++ gms-server/gms-handler/scripts-zh-CN/template/WildBossTemplate.js
@@ -56,18 +56,16 @@ function start() {
 
     try {
         map.spawnMonsterOnGroundBelow(bossObj, point);
-
+        log.info("召唤野外boss: {}, ID:{}", bossObj.getName(), BossID);
         if (isinit) {
-            // 修正后的日志字符串模板
-//            log.info(`[野外BOSS] \({em.getName()} 在频道\){channel} 地图: \({map.getMapName()} (\){MapID}) (\({point.x},\){point.y}) 成功生成 \({bossName} (\){BossID})，下次检测间隔：${timerMs / 60000} 分钟`);
-            log.info(`[事件脚本-野外BOSS] ${em.getName()} 已在频道 ${channel} 的 ${map.getMapName()}(${MapID}) ${point.x} , ${point.y}) 生成 ${BossName}(${BossID})，检测间隔：${timerMs / 60 / 1000} 分钟`);
+            log.info(`[野外BOSS] ({em.getName()} 在频道){channel} 地图:({map.getMapName()}(){MapID}) (({point.x},){point.y}) 成功生成 ({bossName}(){BossID})，下次检测间隔：${timerMs / 60000} 分钟`);
         } else {
             isinit = true;
         }
 
-        // 修正后的广播提示
+        // 广播刷新提示
         if (BossNotice && BossNotice.length > 0) {
-            map.broadcastMessage(PacketCreator.serverNotice(6, `[野外BOSS] ${BossName}  ${BossNotice}`));
+            map.broadcastMessage(PacketCreator.serverNotice(6, `[野外BOSS] ({bossName}){BossNotice}`));
         }
 
     } catch (e) {
